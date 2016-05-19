6916ceca30db4df05630396808883e001ea798fb
JongSeokKim <js68.kim@gmail.com>
2016-05-19 11:18:25 +0900

    first commit

---
+++ app/src/main/java/miracom/com/alarm/MainActivity.java
@@ -0,0 +1,13 @@
+package miracom.com.alarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
