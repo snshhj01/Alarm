45240b4842f1a88d910b7720d2578efff539583e
sw0815.yoon <sw0815.yoon@miracom.co.kr>
2016-05-26 17:11:21 +0900

    Rejected commit: Default

---
+++ app/src/main/java/kr/co/miracom/alarm/activity/AddSite.java
@@ -0,0 +1,18 @@
+package kr.co.miracom.alarm.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.co.miracom.alarm.R;
+
+/**
+ * Created by admin on 2016-05-26.
+ */
+public class AddSite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_site);
+    }
+}
